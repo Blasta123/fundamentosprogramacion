d171079966b25a0d2b2e04023d4e23a85eb260d8
Blasta <nanod44@gmail.com>
2025-01-13 17:35:05 -0300

    primer etapa de curso, vemos tipos de datos simples

---
+++ practicaspythondalto/tipos de datos/tipos- de-datos.py
@@ -0,0 +1,28 @@
+#Declaro una variable
+
+nombre = "David"
+
+#Declaro una variable con camelCase
+
+nombreCompletoDeTuTioMaster = "x"
+
+#Declaro una variable con snake_case (lo que se debe hacer)
+
+nombre_de_tu_tio_master = "x"
+
+#concatenamos strings con +
+
+bienvenida = "Hola " + nombre + " ¿Como estás?"
+
+#concatenamos con F-strings
+
+bienvenida = f"Hola {nombre} ¿como estás?"
+
+#Imprimimos el resultado en pantalla
+
+print (bienvenida);
+
+#operadores de pertenencia (in / not in)
+
+print ("David" in bienvenida) #esto deberia dar true
+print ("David" not in bienvenida) #esto deberia dar false
