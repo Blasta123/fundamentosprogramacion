9b7cbd1cb98bbaa85b2b163278d74432121e50ec
Blasta <nanod44@gmail.com>
2025-01-15 09:46:19 -0300

    if-else y como se aplica

--- practicaspythondalto/tipos de datos/condicionales/if-elf.py
+++ practicaspythondalto/tipos de datos/condicionales/if-elf.py
@@ -1,12 +1,13 @@
-#if-else nos permite comparar datos dando una condición a cumplir para 
+#if-else nos permite comparar datos dando una condición a cumplir para
 #que se ejecute tal o cuál cosa, dependiendo de lo que queramos lograr
 
 contrasena = "12"
 contrasena_escrita = "12"
 
 if contrasena == contrasena_escrita:
-    print(Iniciando Sesión)
+    print("Iniciando Sesión")
 else:
-    print(Contrasena equivocada, intente nuevamente)
+    print("Contrasena equivocada, intente nuevamente")
+
 
 print("Esto está fuera del condicional, siempre se va a mostrar")
